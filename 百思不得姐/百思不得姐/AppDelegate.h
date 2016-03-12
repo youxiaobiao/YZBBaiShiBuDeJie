8015897d553817ba1747e48093f804e24cd43f65
尤佐标 <946448026@qq.com>
2016-03-12 19:44:48 +0800

    开始创建项目

---
+++ 百思不得姐/百思不得姐/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  百思不得姐
+//
+//  Created by 尤佐标 on 16/3/12.
+//  Copyright © 2016年 尤佐标. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 百思不得姐/百思不得姐/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  百思不得姐
+//
+//  Created by 尤佐标 on 16/3/12.
+//  Copyright © 2016年 尤佐标. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    NSLog(@"哈哈哈");
+}
+
+
+
+
+@end
